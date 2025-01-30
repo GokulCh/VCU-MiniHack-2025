fb53521ba005986f8a86df905e200bc0b0432b59
GokulCh <gokul.chaluvadi@gmail.com>
2025-01-30 18:34:28 -0500

    Changed color

--- app/src/components/HomePage/FutureSection.jsx
+++ app/src/components/HomePage/FutureSection.jsx
@@ -4,7 +4,7 @@ const FutureSection = () => {
   return (
     <section id="Future" className="py-16">
       <div className="max-w-6xl mx-auto text-center px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-green-500">Future Works</h2>
+        <h2 className="text-3xl font-bold text-primary">Future Works</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
           <FutureWork
             icon="bx bx-brain"
